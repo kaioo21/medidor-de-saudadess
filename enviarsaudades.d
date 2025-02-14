1478d620257c6b033311795a17e2021b2b3d904c
kaioo21 <121466910+kaioo21@users.noreply.github.com>
2025-02-14 20:08:01 -0300

    Add files via upload

---
+++ enviarsaudades.d
@@ -0,0 +1,11 @@
+const saudadesSlider = document.getElementById('saudadesSlider');
+const saudadesValue = document.getElementById('saudadesValue');
+
+saudadesSlider.addEventListener('input', () => {
+    saudadesValue.textContent = `${saudadesSlider.value}%`;
+});
+
+function enviarSaudades() {
+    const valorSaudades = saudadesSlider.value;
+    alert(`Você enviou ${valorSaudades}% de saudades! 💖`);
+}
